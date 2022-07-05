a9d6ad10de4ded1a734e06a67edde3dc731e3de3
Sourav Banerjee <99480048+souravwithstars@users.noreply.github.com>
2022-07-05 13:00:17 +0530

    Removed event after declared winning
    Separated file for board from game

---
+++ board.js
@@ -0,0 +1,117 @@
+class Board {
+  #wordBlocks;
+  #actual;
+  #typedWords;
+  #typed;
+  #guessedCount;
+  #index;
+  #nextIndex;
+  #correctSpots;
+  #wrongSpots;
+  constructor(wordBlocks, actual) {
+    this.#wordBlocks = wordBlocks;
+    this.#actual = actual;
+    this.#typedWords = [];
+    this.#typed = '';
+    this.#guessedCount = 0;
+    this.#index = 0;
+    this.#nextIndex = 0;
+    this.#correctSpots = [];
+    this.#wrongSpots = [];
+  }
+
+  getDivName() {
+    return this.#wordBlocks[this.#guessedCount];
+  }
+
+  getActual() {
+    return this.#actual;
+  }
+
+  getIndex() {
+    return this.#index;
+  }
+
+  getTypedWord() {
+    return this.#typed;
+  }
+
+  getLastCharacterTyped() {
+    const lastIndex = this.#typed.length - 1;
+    return this.#typed[lastIndex];
+  }
+
+  getGuessedCount() {
+    return this.#guessedCount;
+  }
+
+  getCorrectSpots() {
+    return this.#correctSpots;
+  }
+
+  getWrongSpots() {
+    return this.#wrongSpots;
+  }
+
+  addChar(character) {
+    this.#index = this.#nextIndex;
+    if (this.#index <= 4) {
+      this.#typed += character;
+      this.#nextIndex++;
+      return;
+    }
+    this.#nextIndex = 5;
+  }
+
+  removeChar() {
+    this.#nextIndex--;
+    if (this.#nextIndex <= 0) {
+      this.#index = 0;
+      this.#nextIndex = 0;
+      return;
+    }
+    if (this.#index < 0) {
+      this.#index = 0;
+      this.#nextIndex = 0;
+      return;
+    }
+    this.#index = this.#nextIndex;
+    this.#typed = this.#typed.slice(0, this.#typed.length - 1);
+  }
+
+  validate() {
+    this.#correctSpots = checkCorrect(this.#actual, this.#typed);
+    this.#wrongSpots = checkWrong(
+      this.#actual, this.#typed, this.#correctSpots
+    );
+  }
+
+  changeResources() {
+    this.#typedWords.push(this.getTypedWord);
+    this.#typed = '';
+    this.#guessedCount += 1;
+    this.#nextIndex = 0;
+  }
+}
+
+const checkCorrect = (actual, typed) => {
+  const correctChars = typed.split('').filter((char, index) => char === actual[index]);
+
+  const correctSpots = correctChars.map(char => actual.indexOf(char));
+  return correctSpots;
+};
+
+const checkWrong = (actual, typed, correctSpots) => {
+  const wrongSpots = [];
+  const actualWord = actual.split('');
+  for (let i = 0; i < typed.length; i++) {
+    if (!correctSpots.includes(i)) {
+      for (j = 0; j < typed.length; j++) {
+        if (typed[j] === actual[i]) {
+          wrongSpots.push(j);
+        }
+      }
+    }
+  }
+  return wrongSpots;
+};
--- wordle.js
+++ wordle.js
@@ -1,100 +1,4 @@
 (function () {
-  class Board {
-    #wordBlocks;
-    #actual;
-    #typedWords;
-    #typed;
-    #guessedCount;
-    #index;
-    #nextIndex;
-    #correctSpots;
-    #wrongSpots;
-    constructor(wordBlocks, actual) {
-      this.#wordBlocks = wordBlocks;
-      this.#actual = actual;
-      this.#typedWords = [];
-      this.#typed = '';
-      this.#guessedCount = 0;
-      this.#index = 0;
-      this.#nextIndex = 0;
-      this.#correctSpots = [];
-      this.#wrongSpots = [];
-    }
-
-    getDivName() {
-      return this.#wordBlocks[this.#guessedCount];
-    }
-
-    getActual() {
-      return this.#actual;
-    }
-
-    getIndex() {
-      return this.#index;
-    }
-
-    getTypedWord() {
-      return this.#typed;
-    }
-
-    getLastCharacterTyped() {
-      const lastIndex = this.#typed.length - 1;
-      return this.#typed[lastIndex];
-    }
-
-    getGuessedCount() {
-      return this.#guessedCount;
-    }
-
-    getCorrectSpots() {
-      return this.#correctSpots;
-    }
-
-    getWrongSpots() {
-      return this.#wrongSpots;
-    }
-
-    addChar(character) {
-      this.#index = this.#nextIndex;
-      if (this.#index <= 4) {
-        this.#typed += character;
-        this.#nextIndex++;
-        return;
-      }
-      this.#nextIndex = 5;
-    }
-
-    removeChar() {
-      this.#nextIndex--;
-      if (this.#nextIndex <= 0) {
-        this.#index = 0;
-        this.#nextIndex = 0;
-        return;
-      }
-      if (this.#index < 0) {
-        this.#index = 0;
-        this.#nextIndex = 0;
-        return;
-      }
-      this.#index = this.#nextIndex;
-      this.#typed = this.#typed.slice(0, this.#typed.length - 1);
-    }
-
-    validate() {
-      this.#correctSpots = checkCorrect(this.#actual, this.#typed);
-      this.#wrongSpots = checkWrong(
-        this.#actual, this.#typed, this.#correctSpots
-      );
-    }
-
-    changeResources() {
-      this.#typedWords.push(this.getTypedWord);
-      this.#typed = '';
-      this.#guessedCount += 1;
-      this.#nextIndex = 0;
-    }
-  }
-
   const displayChar = board => {
     const divName = board.getDivName();
     const index = board.getIndex();
@@ -154,32 +58,9 @@
     const correctSpots = board.getCorrectSpots();
     const wrongSpots = board.getWrongSpots();
 
-
     correctSpots.forEach(spot => colorGreen(spot, board));
     wrongSpots.forEach(spot => colorYellow(spot, board));
     colorGrey(correctSpots, wrongSpots, board);
-  };
-
-  const checkCorrect = (actual, typed) => {
-    const correctChars = typed.split('').filter((char, index) => char === actual[index]);
-
-    const correctSpots = correctChars.map(char => actual.indexOf(char));
-    return correctSpots;
-  };
-
-  const checkWrong = (actual, typed, correctSpots) => {
-    const wrongSpots = [];
-    const actualWord = actual.split('');
-    for (let i = 0; i < typed.length; i++) {
-      if (!correctSpots.includes(i)) {
-        for (j = 0; j < typed.length; j++) {
-          if (typed[j] === actual[i]) {
-            wrongSpots.push(i);
-          }
-        }
-      }
-    }
-    return wrongSpots;
   };
 
   const wrongLength = board => {
@@ -203,6 +84,24 @@
     return;
   };
 
+  const removeListeneners = board => {
+    const spans = document.getElementsByClassName('char');
+    [...spans].forEach(span => {
+      span.onclick = null;
+    });
+
+    const backspaceKey = document.getElementById('backspace');
+    backspaceKey.onclick = null;
+
+    const enterKey = document.getElementById('enter');
+    enterKey.onclick = null;
+  };
+
+  const declaredWinner = board => {
+    removeListeneners(board);
+    setTimeout(() => alert('You Guessed The Correct Word'), 1000);
+  };
+
   const validator = board => {
     if (wrongLength(board)) {
       alert('Please Insert 5 Letters Word');
@@ -212,7 +111,7 @@
     makeColor(board);
     board.changeResources();
     if (guessCorrect(board)) {
-      setTimeout(() => alert('You Guessed The Correct Word'), 1000);
+      declaredWinner(board);
       return;
     }
     if (allGuessWrong(board)) {
@@ -221,30 +120,41 @@
     }
   };
 
+  const handleClickForAdd = board => {
+    return event => {
+      const letter = event.target.innerText;
+      board.addChar(letter);
+      displayChar(board);
+    }
+  };
+
+  const handleClickForBackSpace = board => {
+    return event => {
+      board.removeChar();
+      deleteChar(board);
+    }
+  };
+
+  const handleclickForEnter = board => {
+    return event => {
+      validator(board);
+    }
+  };
+
   const main = () => {
     const wordBlocks = ['word-1', 'word-2', 'word-3', 'word-4', 'word-5', 'word-6'];
-    const actual = 'ROMAN';
+    const actual = 'EARTH';
     const board = new Board(wordBlocks, actual);
     const letterSpans = document.getElementsByClassName('char');
-
     [...letterSpans].forEach(span => {
-      span.addEventListener('click', event => {
-        const letter = event.target.innerText;
-        board.addChar(letter);
-        displayChar(board);
-      });
+      span.onclick = handleClickForAdd(board);
     });
 
     const backspaceKey = document.getElementById('backspace');
-    backspaceKey.addEventListener('click', event => {
-      board.removeChar();
-      deleteChar(board);
-    });
+    backspaceKey.onclick = handleClickForBackSpace(board);
 
     const enterKey = document.getElementById('enter');
-    enterKey.addEventListener('click', event => {
-      validator(board);
-    });
+    enterKey.onclick = handleclickForEnter(board);
 
     document.addEventListener('keydown', (event) => {
       const key = event.key.toUpperCase();
